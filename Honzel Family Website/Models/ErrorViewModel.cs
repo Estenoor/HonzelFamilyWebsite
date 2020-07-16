43451fb304a30bc91aab85227a45a278c5614a91
SamzFerg <SamzFerg@gmail.com>
2020-07-16 11:18:40 -0500

    Initial Project Creation
    Generated Initial Project Files

---
+++ Honzel Family Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Honzel_Family_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
